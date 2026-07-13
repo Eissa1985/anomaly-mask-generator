019b528b6b534f25a9b90efea0b0b14d6236de9e
Eissa1985 <82113011+Eissa1985@users.noreply.github.com>
2026-07-13 14:02:25 +0300

    Update losses.py

--- losses.py
+++ losses.py
@@ -3,6 +3,67 @@ import segmentation_models_pytorch.losses as losses
 import torch
 import torch.nn.functional as F
 
+class FocalLoss(nn.Module):
+
+    def __init__(self, smooth=1e-5, gamma=0, alpha=None, size_average=True):
+        super(FocalLoss, self).__init__()
+        self.gamma = gamma
+        self.alpha = alpha
+        self.smooth = smooth
+        if isinstance(alpha, (float, int)): self.alpha = torch.Tensor([alpha, 1 - alpha])
+        if isinstance(alpha, list): self.alpha = torch.Tensor(alpha)
+        self.size_average = size_average
+
+    def forward(self, input, target):
+        if input.dim()>2:
+            input = input.view(input.size(0), input.size(1), -1)  # N,C,H,W => N,C,H*W
+            input = input.transpose(1, 2)                         # N,C,H*W => N,H*W,C
+            input = input.contiguous().view(-1, input.size(2))    # N,H*W,C => N*H*W,C
+        target = target.view(-1, 1)
+
+        pt = input
+        logpt = (pt + 1e-5).log()
+
+        # add label smoothing
+        num_class = input.shape[1]
+        idx = target.cpu().long()
+
+        one_hot_key = torch.FloatTensor(target.size(0), num_class).zero_()
+        one_hot_key = one_hot_key.scatter_(1, idx, 1)
+        if one_hot_key.device != input.device:
+            one_hot_key = one_hot_key.to(input.device)
+
+        if self.smooth:
+            one_hot_key = torch.clamp(
+                one_hot_key, self.smooth, 1.0 - self.smooth)
+            logpt = logpt * one_hot_key
+
+        if self.alpha is not None:
+            if self.alpha.type() != input.data.type():
+                self.alpha = self.alpha.type_as(input.data)
+            at = self.alpha.gather(0, target.data.view(-1))
+            logpt = logpt * at
+
+        loss = (-1 * (1 - pt)**self.gamma * logpt).sum(1)
+        if self.size_average: return loss.mean()
+        else: return loss.sum()
+
+class DiceLoss(nn.Module):
+    def __init__(self, smooth=100.0):
+        super(DiceLoss, self).__init__()
+        self.smooth = smooth
+
+    def forward(self, inputs, targets):
+
+        inputs = inputs.reshape(-1)
+        targets = targets.reshape(-1)
+            
+        intersection = torch.sum(inputs * targets)
+        union = torch.sum(inputs) + torch.sum(targets)
+        dice = (2. * intersection + self.smooth) / (union + self.smooth)
+
+        return 1 - dice
+            
 # class FocalLoss(nn.Module):
 #     def __init__(self, alpha=0.25, gamma=2.0):
 #         super(FocalLoss, self).__init__()
@@ -90,10 +151,10 @@ class IoUOptimizedLoss(nn.Module):
         self.dice_weight = dice_weight
         self.focal_weight = focal_weight
         
-        # self.dice_loss = DiceLoss()
+        self.dice_loss = DiceLoss()
         self.focal_loss = FocalLoss(smooth=1e-4, gamma=0)
-        self.dice_loss = losses.DiceLoss(mode='binary')
-        self.bce_loss = nn.BCEWithLogitsLoss()
+        # self.dice_loss = losses.DiceLoss(mode='binary')
+        self.bce_loss = nn.L1Loss() #nn.BCEWithLogitsLoss()
 
     def forward(self, logits, targets):
         """
@@ -119,37 +180,37 @@ class IoUOptimizedLoss(nn.Module):
         
         return total_loss
         
-class FocalLoss(nn.Module):
+# class FocalLoss(nn.Module):
 
-    def __init__(self, smooth=1e-5, gamma=0, alpha=None, size_average=True):
-        super(FocalLoss, self).__init__()
-        self.gamma = gamma
-        self.alpha = alpha
-        self.smooth = smooth
+#     def __init__(self, smooth=1e-5, gamma=0, alpha=None, size_average=True):
+#         super(FocalLoss, self).__init__()
+#         self.gamma = gamma
+#         self.alpha = alpha
+#         self.smooth = smooth
         
-        if isinstance(alpha, (float, int)): self.alpha = torch.Tensor([alpha, 1 - alpha])
-        if isinstance(alpha, list): self.alpha = torch.Tensor(alpha)
-        self.size_average = size_average
+#         if isinstance(alpha, (float, int)): self.alpha = torch.Tensor([alpha, 1 - alpha])
+#         if isinstance(alpha, list): self.alpha = torch.Tensor(alpha)
+#         self.size_average = size_average
 
     
-    def forward(self, input, target):
-        if input.dim() > 2:
-            input = input.view(input.size(0), input.size(1), -1)
-            input = input.transpose(1, 2)
-            input = input.contiguous().view(-1, input.size(2))
+#     def forward(self, input, target):
+#         if input.dim() > 2:
+#             input = input.view(input.size(0), input.size(1), -1)
+#             input = input.transpose(1, 2)
+#             input = input.contiguous().view(-1, input.size(2))
 
-        target = target.view(-1, 1).long() 
-        lprobs = F.log_softmax(input, dim=1)
+#         target = target.view(-1, 1).long() 
+#         lprobs = F.log_softmax(input, dim=1)
         
-        n_class = lprobs.size(1)
+#         n_class = lprobs.size(1)
         
-        one_hot = torch.zeros(target.size(0), n_class).to(input.device)
-        one_hot.scatter_(1, target, 1)
+#         one_hot = torch.zeros(target.size(0), n_class).to(input.device)
+#         one_hot.scatter_(1, target, 1)
 
-        smooth_target = (1 - self.smooth) * one_hot + self.smooth / n_class
-        p_t = torch.exp(lprobs.gather(1, target)) 
-        focal_loss = -1 * (1 - p_t) ** self.gamma * torch.log(p_t)
-        return focal_loss.mean()
+#         smooth_target = (1 - self.smooth) * one_hot + self.smooth / n_class
+#         p_t = torch.exp(lprobs.gather(1, target)) 
+#         focal_loss = -1 * (1 - p_t) ** self.gamma * torch.log(p_t)
+#         return focal_loss.mean()
 
 
 # class CompositeLoss(nn.Module):
